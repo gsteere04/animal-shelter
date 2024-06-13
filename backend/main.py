5f34ded366dd723ed012a5debac4650975c1816b
Greg Anderson <greg.anderson.cs@gmail.com>
2024-06-13 13:04:29 -0600

    Update json pseudo database to be a `list[Shelter]`

--- backend/main.py
+++ backend/main.py
@@ -6,55 +6,48 @@ from schemas import Animals, Shelter
 # Make the pydantic model `Shelter` that will represent this data, then manually
 # change this list to be a `list[Shelter]`. You don't need to write code to convert
 # this list, just manually change it by hand.
-shelters_json: list = [
-    {
-        "name": "St. George Animal Shelter",
-        "address": "605 Waterworks Dr, St. George, UT 84770",
-        "animals": {
-            "cats": 13,
-            "dogs": 15,
-        },
-    },
-    {
-        "name": "St. George Paws",
-        "address": "1125 W 1130 N, St. George, UT 84770",
-        "animals": {
-            "cats": 12,
-            "dogs": 9,
-        },
-    },
-    {
-        "name": "Animal Rescue Team",
-        "address": "1838 W 1020 N Ste. B, St. George, UT 84770",
-        "animals": {
-            "cats": 4,
-            "dogs": 7,
-        },
-    },
-    {
-        "name": "Bailey's Rescued Animals",
-        "address": "25 Main St, St. George, UT 84770",
-        "animals": {
-            "cats": 0,
-            "dogs": 0,
-        },
-    },
-    {
-        "name": "Izzy's Home for Cute Cats",
-        "address": "28 Main St, St. George, UT 84770",
-        "animals": {
-            "cats": 10,
-            "dogs": 0,
-        },
-    },
+shelters: list[Shelter] = [
+    Shelter(
+        name="St. George Animal Shelter",
+        address="605 Waterworks Dr, St. George, UT 84770",
+        animals=Animals(
+            cats=13,
+            dogs=15,
+        ),
+    ),
+    Shelter(
+        name="St. George Paws",
+        address="1125 W 1130 N, St. George, UT 84770",
+        animals=Animals(
+            cats=12,
+            dogs=9,
+        ),
+    ),
+    Shelter(
+        name="Animal Rescue Team",
+        address="1838 W 1020 N Ste. B, St. George, UT 84770",
+        animals=Animals(
+            cats=4,
+            dogs=7,
+        ),
+    ),
+    Shelter(
+        name="Bailey's Rescued Animals",
+        address="25 Main St, St. George, UT 84770",
+        animals=Animals(
+            cats=0,
+            dogs=0,
+        ),
+    ),
+    Shelter(
+        name="Izzy's Home for Cute Cats",
+        address="28 Main St, St. George, UT 84770",
+        animals=Animals(
+            cats=10,
+            dogs=0,
+        ),
+    ),
 ]
-
-shelters: list[Shelter] = []
-for shelter in shelters_json:
-    animals = Animals(cats=shelter["animals"]["cats"], dogs=shelter["animals"]["dogs"])
-    shelters.append(
-        Shelter(name=shelter["name"], address=shelter["address"], animals=animals)
-    )
 
 app = FastAPI()
 
